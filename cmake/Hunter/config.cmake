a589411610710a8a806ef2671deddba883863461
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2019-12-05 13:36:27 +0000

    cmake hunter support.

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,3 @@
+# Specific hunter dependency version pins and custom config
+
+#hunter_config(asio URL "https://github.com/ned14/asio/tarball/asio-1-12-branch" SHA1 "d8f8461745c14e172757d3466cd8db7ae6559fdc")
---
+++ cmake/HunterVersion.cmake
@@ -0,0 +1,6 @@
+include("cmake/HunterGate.cmake")
+HunterGate(
+    URL "https://github.com/cpp-pm/hunter/archive/v0.23.254.tar.gz"
+    SHA1 "017e688b9848c1f695303e8cfe9f35d40e154112"
+    LOCAL
+)
